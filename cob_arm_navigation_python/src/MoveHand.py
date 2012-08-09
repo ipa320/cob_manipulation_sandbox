9d705fecff452aadd78dd988af9ecccf21181462
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2012-08-09 17:27:13 +0200

    added AttachObject

--- cob_arm_navigation_python/src/MoveHand.py
+++ cob_arm_navigation_python/src/MoveHand.py
@@ -3,6 +3,7 @@ roslib.load_manifest('cob_arm_navigation_python')
 
 from MotionPlan import *
 import simple_script_server
+from arm_navigation_msgs.msg import *
 import PlanningScene
 
 class MoveHand(MotionExecutable):
@@ -35,3 +36,53 @@ class MoveHand(MotionExecutable):
     def execute(self):
         return robot.attach_object(name, self.graspable_object, grasp_configuration)      
 """
+class AttachObject:
+    def __init__(self,hand, object_id):
+        self.hand = hand
+        self.object_id = object_id
+        self._attached_object_pub = rospy.Publisher('attached_collision_object', AttachedCollisionObject)
+        self._object_pub = rospy.Publisher('collision_object', CollisionObject)
+    def plan(self, planning_scene = None):
+        if planning_scene == None:
+            planning_scene = PlanningScene.PlanningScene()
+        #print "CURRENT", planning_scene.get_current_scene()
+        if not planning_scene.attach_and_remove_object(self.hand, self.object_id):
+            return ErrorCode("Collision object " + self.object_id + " does not exist")
+        return ErrorCode()
+    def execute(self):
+        planning_scene = PlanningScene.PlanningScene()
+        aobj = AttachedCollisionObject()
+        aobj.link_name = "sdh_palm_link"
+        aobj.object = planning_scene.get_collision_object(self.object_id)
+        aobj.object.operation.operation = aobj.object.operation.ATTACH_AND_REMOVE_AS_OBJECT
+        aobj.touch_links = ['sdh_palm_link','sdh_finger_11_link','sdh_finger_12_link','sdh_finger_13_link','sdh_finger_21_link','sdh_finger_22_link','sdh_finger_23_link','sdh_thumb_1_link','sdh_thumb_2_link','sdh_thumb_3_link']
+        self._attached_object_pub.publish(aobj)
+        rospy.sleep(0.5)
+        return MotionHandleDummy()
+class EnableCollision:
+    def __init__(self, object1, object2):
+        self.object1 = object1
+        self.object2 = object2
+    def plan(self, planning_scene = None):
+        if planning_scene == None:
+            planning_scene = PlanningScene.PlanningScene()
+        planning_scene.enable_collision(self.object1, self.object2)
+        #print planning_scene.get_current_scene()
+        return ErrorCode()
+    def execute(self):
+        planning_scene = PlanningScene.PlanningScene()
+        planning_scene.enable_collision(self.object1, self.object2)
+        return MotionHandleDummy()
+    
+class ResetCollisions:
+    def plan(self, planning_scene = None):
+        if planning_scene == None:
+            planning_scene = PlanningScene.PlanningScene()
+        planning_scene.reset_collisions()
+        return ErrorCode()
+    def execute(self):
+        planning_scene = PlanningScene.PlanningScene()
+        planning_scene.reset_collisions()
+        return MotionHandleDummy()
+            
+     
--- cob_arm_navigation_python/src/PlanningScene.py
+++ cob_arm_navigation_python/src/PlanningScene.py
@@ -4,6 +4,9 @@ import rospy
 
 from arm_navigation_msgs.srv import *
 from arm_navigation_msgs.msg import *
+from geometry_msgs.msg import *
+from copy import deepcopy
+from tf.transformations import *
 
 import threading
 
@@ -33,3 +36,50 @@ class PlanningScene:
     def set_robot_state(self, rs):
         self.scene_diff.planning_scene_diff.robot_state = rs
         self.write_scene(self.scene_diff)
+	
+    def get_collision_object(self, object_id,scene = None):
+	if scene is None:
+	    scene = self.get_current_scene()
+	obj = None
+	for c in scene.collision_objects:
+	    if c.id == object_id:
+		obj = c
+		break
+	#print scene
+	return obj
+    def attach_and_remove_object(self, hand, object_id):
+	obj = self.get_collision_object(object_id)
+	if obj is None:
+	    return False
+	obj.operation.operation = obj.operation.REMOVE
+	with self.lock:
+	    self.scene_diff.planning_scene_diff.collision_objects.append(obj)
+	aobj = AttachedCollisionObject()
+	aobj.link_name = "sdh_palm_link"
+	aobj.object = deepcopy(obj)
+	aobj.object.operation.operation = aobj.object.operation.ADD
+	aobj.touch_links = ['sdh_palm_link','sdh_finger_11_link','sdh_finger_12_link','sdh_finger_13_link','sdh_finger_21_link','sdh_finger_22_link','sdh_finger_23_link','sdh_thumb_1_link','sdh_thumb_2_link','sdh_thumb_3_link']
+	#TODO: Transform
+	aobj.object.header.frame_id = "sdh_palm_link"
+	aobj.object.poses[0].position = Point(0,0,0)
+	aobj.object.poses[0].orientation = Quaternion(*quaternion_from_euler(-1.581, -0.019, 2.379))
+	with self.lock:
+	    self.scene_diff.planning_scene_diff.attached_collision_objects.append(aobj)
+	self.get_current_scene()
+	return True
+    def reset_collisions(self):
+	with self.lock:
+	    self.scene_diff.operations = OrderedCollisionOperations()
+	self.get_current_scene()
+    def enable_collision(self,object1, object2):
+	op = CollisionOperation()
+	op.object1 = object1
+	op.object2 = object2
+	op.operation = op.DISABLE
+	with self.lock:
+	    self.scene_diff.operations.collision_operations.append(op)
+	print self.get_current_scene()
+	rospy.sleep(1.0)
+
+
+
