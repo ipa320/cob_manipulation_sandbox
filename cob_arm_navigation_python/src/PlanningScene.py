b5fe2f4fd0b43562dc4ee39ab27bec67f7519bf5
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2012-08-10 13:57:23 +0200

    attach with transform -> dependency to pr2_python

--- cob_arm_navigation_python/src/PlanningScene.py
+++ cob_arm_navigation_python/src/PlanningScene.py
@@ -7,20 +7,32 @@ from arm_navigation_msgs.msg import *
 from geometry_msgs.msg import *
 from copy import deepcopy
 from tf.transformations import *
+from pr2_python.transform_state import TransformState
 
 import threading
+
 
 class PlanningScene:
     def __init__(self):
 	self.scene_diff = SetPlanningSceneDiffRequest()
 	self.write_scene = rospy.ServiceProxy('/environment_server/set_planning_scene_diff', SetPlanningSceneDiff)
+	self.get_scene = rospy.ServiceProxy('/environment_server/get_planning_scene', GetPlanningScene)
 	self.lock = threading.Lock()
-	self.get_current_scene()
-    
+	self.write_current_scene()
+	self.transformer = TransformState()
+
     def get_current_scene(self):
 	with self.lock:
+	    return self.get_scene(GetPlanningSceneRequest(self.scene_diff.planning_scene_diff,self.scene_diff.operations)).planning_scene
+    def write_current_scene(self):
+	with self.lock:
 	    return self.write_scene(self.scene_diff).planning_scene
-    
+	rospy.sleep(1.0)
+    def get_robot_state(self):
+	return self.get_current_scene().robot_state
+    def transform_pose(self, new_frame_id, old_frame_id, pose):
+	return self.transformer.transform_pose(new_frame_id, old_frame_id, pose, self.get_robot_state())
+	
     def set_joint_state(self, names, positions):
 	with self.lock:
 	    self.scene_diff.planning_scene_diff.robot_state.joint_state.name = list(self.scene_diff.planning_scene_diff.robot_state.joint_state.name)
@@ -59,18 +71,17 @@ class PlanningScene:
 	aobj.object = deepcopy(obj)
 	aobj.object.operation.operation = aobj.object.operation.ADD
 	aobj.touch_links = ['sdh_palm_link','sdh_finger_11_link','sdh_finger_12_link','sdh_finger_13_link','sdh_finger_21_link','sdh_finger_22_link','sdh_finger_23_link','sdh_thumb_1_link','sdh_thumb_2_link','sdh_thumb_3_link']
-	#TODO: Transform
+	for i in range(len(aobj.object.poses)):
+	    aobj.object.poses[i] = self.transform_pose("sdh_palm_link",aobj.object.header.frame_id,aobj.object.poses[i])
 	aobj.object.header.frame_id = "sdh_palm_link"
-	aobj.object.poses[0].position = Point(0,0,0)
-	aobj.object.poses[0].orientation = Quaternion(*quaternion_from_euler(-1.581, -0.019, 2.379))
 	with self.lock:
 	    self.scene_diff.planning_scene_diff.attached_collision_objects.append(aobj)
-	self.get_current_scene()
+	self.write_current_scene()
 	return True
     def reset_collisions(self):
 	with self.lock:
 	    self.scene_diff.operations = OrderedCollisionOperations()
-	self.get_current_scene()
+	self.write_current_scene()
     def enable_collision(self,object1, object2):
 	op = CollisionOperation()
 	op.object1 = object1
@@ -78,8 +89,7 @@ class PlanningScene:
 	op.operation = op.DISABLE
 	with self.lock:
 	    self.scene_diff.operations.collision_operations.append(op)
-	print self.get_current_scene()
-	rospy.sleep(1.0)
+	print self.write_current_scene()
 
 
 
