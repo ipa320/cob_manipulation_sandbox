7e64c6422018fa63a99559d2fe14e7249f9586d3
ipa-fxm <felix.messmer@ipa.fhg.de>
2012-08-13 16:08:52 +0200

    added call to /addWorld and /cob_object_handler/handle_object to load_scene

--- cob_manipulation_test/scripts/load_scene.py
+++ cob_manipulation_test/scripts/load_scene.py
@@ -6,6 +6,7 @@ from arm_navigation_msgs.msg import *
 from arm_navigation_msgs.srv import *
 from geometry_msgs.msg import *
 from tf.transformations import *
+from cob_object_handler.srv import *
 
 def createObject(shapetype, name, pos, euler, extent):
     co = CollisionObject()
@@ -27,10 +28,36 @@ def createObject(shapetype, name, pos, euler, extent):
 if __name__ == '__main__':
     rospy.init_node('load_scene')
     while rospy.get_time() == 0.0: pass
+    
+    print "Waiting for Service 'addWorld'"
+    rospy.wait_for_service('addWorld')
+    print "...done"
+    try:
+        addWorld = rospy.ServiceProxy('addWorld', HandleObject)
+        req = HandleObjectRequest()
+        req.operation = "add"
+        res = addWorld(req)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+    print "world added"
+        
+    print "aiting for Service 'handle_object'"
+    rospy.wait_for_service('/object_handler/handle_object')
+    print "...done"
+    try:
+        addObject = rospy.ServiceProxy('/object_handler/handle_object', HandleObject)
+        req = HandleObjectRequest()
+        req.operation = "add"
+        req.object = "table_ikea"
+        res = addObject(req)
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+    print "table_ikea added"
+    
     pub = rospy.Publisher('/collision_object', CollisionObject)
     rospy.sleep(1.0)
-    pub.publish( createObject(Shape.BOX, "table_ikea", [-0.9,0,0.4],[0,0,0],[0.4, 0.8, 0.8]) )
-    rospy.sleep(1.0)
+    #pub.publish( createObject(Shape.BOX, "table_ikea", [-0.9,0,0.4],[0,0,0],[0.4, 0.8, 0.8]) )
+    #rospy.sleep(1.0)
     pub.publish( createObject(Shape.CYLINDER, "milk", [-0.8,-0.2,0.9],[0,0,0],[0.05, 0.2]) )
     rospy.sleep(1.0)
     print rospy.ServiceProxy('/environment_server/set_planning_scene_diff', SetPlanningSceneDiff)(SetPlanningSceneDiffRequest())
