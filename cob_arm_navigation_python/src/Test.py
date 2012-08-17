467635a5a891cd5f4671550bdf25cd0b3955fc70
Felix Messmer <fxm@cob3-3-pc1.(none)>
2012-08-17 08:51:35 +0200

    fancy script now even fancier

--- cob_arm_navigation_python/src/Test.py
+++ cob_arm_navigation_python/src/Test.py
@@ -11,11 +11,12 @@ from copy import deepcopy
 rospy.init_node("test")
 
 grasp_pose = PoseStamped()
+#grasp_pose.header.frame_id = "base_link"
 grasp_pose.header.frame_id = "odom_combined"
 grasp_pose.header.stamp = rospy.Time()
 
 p = grasp_pose.pose.position
-p.x, p.y, p.z = [-0.8,-0.2,0.9]
+p.x, p.y, p.z = [-0.9,0.0,0.9]
 p.x += 0.02
 p.y += 0.02
 p.z += 0.02
@@ -25,7 +26,7 @@ o.x,o.y,o.z,o.w = quaternion_from_euler(-1.581, -0.019, 2.379)
 lift_pose = deepcopy(grasp_pose)
 p = lift_pose.pose.position
 p.x += 0.0
-p.y += 0.0
+p.y += -0.05
 p.z += 0.03
 
 mp = MotionPlan()
@@ -38,11 +39,26 @@ mp += AttachObject('arm',  'milk')
 mp += EnableCollision('milk','table_ikea')
 mp += MoveArm("arm",[lift_pose,['sdh_grasp_link']])
 mp += ResetCollisions()
-mp += MoveArm('arm', 'hold')
+#mp += MoveArm('arm', 'home')
+#mp += MoveComponent('tray', 'up')
+#mp += MoveArm('arm', 'hold')
+mp += MoveArm('arm', [['base_link',[0.64-0.161, -0.11+0.056,0.832+0.3],[-1.441, 0.118, -0.078]],['sdh_grasp_link']])
 mp += MoveComponent('tray', 'up')
-mp += MoveArm('arm', [['base_link',[0.64-0.161, -0.11+0.056,0.832+0.25],[-1.441, 0.118, -0.078]],['sdh_grasp_link']])
 mp += MoveComponent('sdh', 'cylopen')
 mp += DetachObject('arm',  'milk')
+mp += EnableCollision('milk','tray_link')
+#mp += MoveArm('arm', 'home')
+mp += MoveArm('arm', [['base_link',[0.64-0.161, -0.11+0.056,0.832+0.22],[-1.441, 0.118, -0.078]],['sdh_grasp_link']])
+mp += MoveComponent('sdh', 'cylclosed')
+mp += AttachObject('arm',  'milk')
+mp += EnableCollision('milk','tray_link')
+mp += MoveArm('arm', [['base_link',[0.64-0.161, -0.11+0.056,0.832+0.5],[-1.441, 0.118, -0.078]],['sdh_grasp_link']])
+mp += ResetCollisions()
+mp += MoveArm('arm', 'home')
+mp += MoveArm("arm",[lift_pose,['sdh_grasp_link']])
+mp += MoveComponent('sdh', 'cylopen')
+mp += DetachObject('arm',  'milk')
+mp += MoveArm('arm', 'pregrasp')
 
 print mp.plan(2)
 for e in mp.execute():
